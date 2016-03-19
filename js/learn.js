015308e43430896332fe6c37e50946222081d39b
Yan Hong <onlyhongyan@gmail.com>
2016-03-19 10:31:58 -0700

    Variables
    Define variables

---
+++ js/learn.js
@@ -0,0 +1,43 @@
+// VARIABLES
+
+// Define a integer variable
+var a = 1;
+
+// Define a string variable
+var a = "This is a string";
+var a = "1";
+
+// Define a float variable
+var a = 1.5;
+var a = .5;
+
+// Define a boolean variable
+var a = true;
+var a = false;
+
+// Define a array
+var arr = [1, 2, 3, 5, 8];
+var arr = ["Hello", "World"];
+var arr = [1, 3, "Hey", false];
+
+// Define a object
+var obj = {
+  color: "#FF0000",
+  length: 15,
+  width: 5.5,
+  name: "A Nice Cube",
+  display: true
+};
+
+// Three empty datatype
+var n = null; // It is nothing inside
+var u = undefined; // It is no defined
+var na = NaN; // It is a irregular number
+
+// Nested arrary and object
+var arr = [1, 2, { width: 10, height: 20}];
+var arr = [1, 2, ["hello", "world"]];
+var obj = {
+  name: "team",
+  members: ["yan", "neo", "anthony"]
+};
